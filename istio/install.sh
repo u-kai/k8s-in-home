b089abf1ea5ee74a082845c905b71efb2e66c806
u-kai <76635578+u-kai@users.noreply.github.com>
2024-02-10 23:31:31 +0900

    add istio

--- istio/install.sh
+++ istio/install.sh
@@ -4,3 +4,5 @@ helm install istio-base istio/base -n istio-system --set defaultRevision=default
 helm install istiod istio/istiod -n istio-system --wait
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.20/samples/addons/jaeger.yaml -n istio-system
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.20/samples/addons/kiali.yaml -n istio-system
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.20/samples/addons/prometheus.yaml -n istio-system
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.20/samples/addons/loki.yaml -n istio-system
