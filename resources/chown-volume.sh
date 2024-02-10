fe8410f48ff2ac7886732256f6bbf023029eca9a
u-kai <76635578+u-kai@users.noreply.github.com>
2024-02-10 20:25:47 +0900

    fix nfs

---
+++ resources/chown-volume.sh
@@ -0,0 +1,2 @@
+sudo chown -R 1001:1001 /media/pi/ex-ssd
+
--- setup/nfs-master.sh
+++ setup/nfs-master.sh
@@ -4,3 +4,4 @@ sudo apt install nfs-kernel-server
 echo "TARGET_DIR  $IP_RANGE(rw,async,crossmnt,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 sudo systemctl start nfs-server.service
 sudo systemctl enable nfs-server.service
+sudo exportfs -ra
