3eff09588abcb961b50c6c7fcccd6d3133050207
u-kai <76635578+u-kai@users.noreply.github.com>
2024-02-11 15:22:20 +0900

    fix schema and test env

--- services/wordbook/create-db-secret.sh
+++ services/wordbook/create-db-secret.sh
@@ -1,4 +1,6 @@
 PASSWORD=$(openssl rand -base64 32 | tr '+/' '-_' | tr -d '=')
-sed "s/SECRET/'"$PASSWORD"'/g" services/wordbook/db-secret.yaml | kubectl apply -f  - -n dev
+sed "s/SECRET/'"$PASSWORD"'/g" services/wordbook/db-secret.yaml | kubectl create -f  - -n dev
+
 PASSWORD=$(openssl rand -base64 32 | tr '+/' '-_' | tr -d '=')
-sed "s/SECRET/$PASSWORD/g" services/wordbook/db-secret.yaml | kubectl apply -f  - -n prod
+sed "s/SECRET/$PASSWORD/g" services/wordbook/db-secret.yaml | kubectl create -f  - -n prod
+
