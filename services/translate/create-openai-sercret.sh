61014047d928c26759c25dc9dc39a3cea304f079
u-kai <76635578+u-kai@users.noreply.github.com>
2024-10-18 07:50:46 +0900

    fix db password ref

--- services/translate/create-openai-sercret.sh
+++
@@ -1,3 +0,0 @@
-sed "s/SECRET/$OPENAI_API_KEY/g" services/translate/openai-secret.yaml | kubectl create -f  - -n test
-sed "s/SECRET/$OPENAI_API_KEY/g" services/translate/openai-secret.yaml | kubectl create -f  - -n dev
-sed "s/SECRET/"$OPENAI_API_KEY"/g" services/translate/openai-secret.yaml | kubectl create -f  - -n prod
--- services/wordbook/delete-pv-and-pvc.sh
+++
@@ -1,5 +0,0 @@
-kubectl patch pv wordbook-db-pv-dev -p '{"metadata":{"finalizers":null}}'
-kubectl patch pvc wordbook-db-pv -p '{"metadata":{"finalizers":null}}' -n dev
-kubectl delete pvc wordbook-db-pv -n dev
-kubectl delete pv wordbook-db-pv-dev
-kubectl apply -f services/wordbook/pv.yaml
